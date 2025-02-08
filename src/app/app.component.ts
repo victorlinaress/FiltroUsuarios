c8b3625c843fc6439f957a18e9f55015052b2504
unknown <victorbr8@outlook.com>
2025-02-08 15:49:34 -0300

    filtro status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,20 +27,56 @@ export class AppComponent implements OnInit {
     this.showUserDetails = true;
   }
 
-  onFilter(filterOptions: IFilterOptions) {  // recebendo o objeto de filtro
+  onFilter(filterOptions: IFilterOptions) {
+    // recebendo o objeto de filtro enviado pelo componente filho
     console.log(filterOptions);
 
     // executar a lógica de filtro e retornar a lista filtrada
     this.userListFiltered = this.filterUsersList(filterOptions, this.usersList);
   }
 
+  // método que executa todos os filtros e retorna a lista filtrada
   filterUsersList(filterOptions: IFilterOptions, usersList: IUser[]): IUser[] {
     let filteredList: IUser[] = [];
+
+    //  aplicando o filtro pelo nome (
     filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+    filteredList = this.filterUsersListStatus(
+      filterOptions.status,
+      filteredList
+    );
+
+    return filteredList; // retorna a lista filtrada
+  }
+  filterUsersListStatus(
+    status: boolean | undefined,
+    usersList: IUser[]
+  ): IUser[] {
+    const STATUS_NOT_SELECTED = status === undefined;
+
+    if (STATUS_NOT_SELECTED) {
+      return usersList;
+    }
+    const filteredList = usersList.filter((user) => user.ativo === status);
+
     return filteredList;
   }
 
+  // método específico para filtrar a lista de usuários pelo nome
   filterUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
-    throw new Error('Method not implemented.');
+    // verifica se o nome foi digitado ou não
+    const NAME_NOT_TYPED = name === undefined;
+
+    // se o nome não foi digitado, retorna a lista original sem filtro
+    if (NAME_NOT_TYPED) {
+      return usersList;
+    }
+
+    // fltra a lista de usuários verificando se o nome digitado está contido no nome do usuário
+    const filteredList = usersList.filter((user) =>
+      user.nome.toLowerCase().includes(name.toLowerCase())
+    );
+
+    return filteredList; // retorna a lista filtrada com base no nome digitado
   }
 }
